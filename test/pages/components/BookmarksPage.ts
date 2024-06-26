c659ce03d56a803447b60e1d3f0382987ecb0ae3
TomRus <rusiecki.tomas@gmail.com>
2024-06-26 12:40:40 +0200

    next changes in bookmarks

--- test/pages/components/BookmarksPage.ts
+++ test/pages/components/BookmarksPage.ts
@@ -3,12 +3,53 @@ class BookmarksPage {
         return $("nav.main-menu li a[href*='/kategorie/bestsellery']")
     }
 
-    async clickOnBookmarkBestsellery() {
-        const icon: WebdriverIO.Element = await this.selectBestsellery;
-        await icon.waitForDisplayed();
-        await icon.click();
+    get selectFromList() {
+        return $("div.change-best-list label[data-seolink='devops']")
     }
-   
+
+    get titleBookDevops() {
+        return $("div.bestseller-list h2 a.vgrafa-link");
+    }
+
+    get addTitleDevopsBook() {
+        return $("div.bestseller-list h2 a.vgrafa-link");
+    }
+
+    get titleBookDevopsFromCart() {
+        return $("div.title-group > h1 > span");
+    }
+
+    async getTitleDevopsBookFromCart(): Promise<string> {
+        const titleBookDevopCart: WebdriverIO.Element = await this.titleBookDevopsFromCart;
+        await titleBookDevopCart.waitForDisplayed();
+        return await this.titleBookDevopsFromCart.getText();
+    }
+
+    async addTitleDevopsBookToCart() {
+        const selectnumber: WebdriverIO.Element = await this.addTitleDevopsBook;
+        await selectnumber.waitForDisplayed();
+        await selectnumber.click();
+    }
+
+
+    async getTitleDevopsBook(): Promise<string> {
+        const titleBookDevop: WebdriverIO.Element = await this.titleBookDevops;
+        await titleBookDevop.waitForDisplayed();
+        return await titleBookDevop.getText();
+    }
+
+
+    async clickOnDevops() {
+        const mark: WebdriverIO.Element = await this.selectFromList
+        await mark.click();
+    }
+
+    async clickOnBookmarkBestsellery() {
+        const selectBook: WebdriverIO.Element = await this.selectBestsellery;
+        await selectBook.waitForDisplayed();
+        await selectBook.click();
+    }
+
 }
 
 export default new BookmarksPage();
--- test/specs/e2e/MainBookmarks.ts
+++ test/specs/e2e/MainBookmarks.ts
@@ -3,7 +3,8 @@ import GlobalPage from "../../pages/GlobalPage";
 import BookmarksPage from "../../pages/components/BookmarksPage";
 
 describe("Bestsellery web site", async () => {
-   
+    let titleDevops: string = "";
+    let titleDevopsCart: string = "";
     it("should click on bookmark Bestsellery in Main Page and verify site", async () => {
         await GlobalPage.openPage(helionHomeUrl, helionHomeUrl);
         await expect(browser).toHaveUrl(helionHomeUrl);
@@ -11,8 +12,19 @@ describe("Bestsellery web site", async () => {
         await BookmarksPage.clickOnBookmarkBestsellery();
         await browser.pause(100);
         await expect(browser).toHaveUrl(bestselleryPage);
-        await browser.pause(100);      
+        await browser.pause(100);
     })
 
-    
+    it("select from list bookmarks Devops and add to cart and finally check the title", async () => {
+        await BookmarksPage.clickOnDevops();
+        titleDevops = await BookmarksPage.getTitleDevopsBook();
+        console.log(titleDevops);
+        await browser.pause(1000);
+        await BookmarksPage.addTitleDevopsBookToCart();
+        await browser.pause(1000);
+        titleDevopsCart = await BookmarksPage.getTitleDevopsBookFromCart();
+        console.log(await BookmarksPage.getTitleDevopsBookFromCart());
+        await expect(await titleDevops).toContain(titleDevopsCart)
+    })
+
 })
